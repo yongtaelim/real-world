7041f4140f6eac1da4a561e3c59cb72a59292b37
임용태 <limyongtae@im-yongtaeui-MacBookPro.local>
2020-08-07 10:08:04 +0900

    LSP stury..

---
+++ src/main/java/com/example/realworld/document/LetterImporter.java
@@ -0,0 +1,11 @@
+package com.example.realworld.document;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LetterImporter implements Importer {
+    @Override
+    public Document importFile(File file) throws IOException {
+        return null;
+    }
+}
