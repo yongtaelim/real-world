6ede04c3ab7f487078181116510902f963cc5dc8
limyongtae <lyt032410@gmail.com>
2020-08-08 15:15:07 +0900

    ISP studying

--- src/main/java/com/example/realworld/document/DocumentManagementSystem.java
+++ src/main/java/com/example/realworld/document/DocumentManagementSystem.java
@@ -1,15 +1,8 @@
 package com.example.realworld.document;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class DocumentManagementSystem {
-    private final Map<String, Importer> extensionToImporter = new HashMap<>();
-
-    public DocumentManagementSystem() {
-        extensionToImporter.put("letter", new LetterImporter());
-        extensionToImporter.put("report", new ReportImporter());
-        extensionToImporter.put("jpg", new ImageImporter());
-    }
-
+	
+	public void importFile(String path) {
+		
+	}
 }
